1149120f4c2ee064e7e68ca585c85327961726a7
Michael <tianzhenyun@gmail.com>
2022-06-27 21:20:12 +0800

    Update _worker.js
    change the doname.

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
+        url.hostname="knit.bid";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
