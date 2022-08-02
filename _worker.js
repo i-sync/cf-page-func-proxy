ee2498cbc3fa271a340bce7445b30f602abc63d1
Michael <tianzhenyun@gmail.com>
2022-08-02 21:43:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="xx-origin.knit.bid";
+        url.hostname="img.viagle.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
